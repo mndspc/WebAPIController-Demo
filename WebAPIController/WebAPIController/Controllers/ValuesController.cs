fd9b13973f6f5b1d4f2b7c86f0ff8efc9ba7a75d
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-28 19:11:16 +0530

    Pushing new project

---
+++ WebAPIController/WebAPIController/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Collections;
+namespace WebAPIController.Controllers
+{
+    public class ValuesController : ApiController
+    {
+       static List<string> strings = new List<string> {"str1","str2","str3","str4","str5" };
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return strings;
+        }
+
+        // GET api/<controller>/5
+        public string Get([FromUri]int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+            strings.Add(value);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);
+        }
+    }
+}
---
+++ WebAPIController/WebAPIController/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIController.WebApiApplication" Language="C#" %>
